16357344cbd4818db0fca19ef3a6f76a9c67bc49
Tambini <tambini.c@gmail.com>
2020-02-17 11:22:38 -0500

    Updated the api-helper for recipes

--- FE/client/src/services/api_helper.js
+++ FE/client/src/services/api_helper.js
@@ -11,25 +11,18 @@ export const loginUser = async (loginData) => {
   console.log(resp);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
   localStorage.setItem('authToken', resp.data.auth_token);
-  // localStorage.setItem('name', resp.data.user.name);
-  // localStorage.setItem('email', resp.data.user.email);
   return resp.data.user;
 }
 
 export const registerUser = async (registerData) => {
-  // try {
+
   const resp = await api.post('/register', registerData);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
   localStorage.setItem('authToken', resp.data.auth_token);
   localStorage.setItem('name', resp.data.user.name);
   localStorage.setItem('email', resp.data.user.email);
   return resp.data.user;
-  // } catch (e) {
-  //   console.log(e.response);
-  //   if (e.response.status === 422) {
-  //     return { errorMessage: "E-mail is already taken please use a new e-mail address" }
-  //   }
-  // }
+
 }
 
 export const verifyUser = () => {
@@ -38,4 +31,27 @@ export const verifyUser = () => {
     api.defaults.headers.common.authorization = `Bearer ${token}`;
   }
 }
+
+
+export const allRecipes = async () => {
+  const resp = await api.get(`/categories/${id}/recipes`);
+  return resp.data;
+};
+
+
+export const getRecipe = async (id) => {
+  const resp = away.api.get(`/categories/${id}/recipes/${id}`)
+  return resp.data
+}
+
+export const createRecipe = async recipeData => {
+  const resp = await api.post(`/categories/${id}/recipes`, recipeData);
+  return resp.data;
+};
+
+
+export const updatePost = async (id, recipeData) => {
+  const resp = await api.put(`/categories/${id}/recipes/${id}`, recipeData);
+  return resp
+};
 
