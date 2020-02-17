73135ba5d06441ba34049284c58ef4bb87e5d628
Tambini <tambini.c@gmail.com>
2020-02-17 09:48:17 -0500

    adding new component

---
+++ FE/client/src/components/CreateRecipe.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+class CreateRecipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ""
+    };
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          this.props.createRecipe(this.state);
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">Image</label>
+        <input
+          type="image"
+          name="image"
+          value={this.state.image}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">Ingredients</label>
+        <input
+          type="text"
+          name="ingredients"
+          value={this.state.ingredients}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="title">Description</label>
+        <input
+          type="text"
+          name="title"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default CreatePostForm;
