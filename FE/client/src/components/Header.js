708e7a3bcd1b96fbddc0b12f2a009da22b67c90c
Tambini <tambini.c@gmail.com>
2020-02-18 15:22:26 -0500

    moved teh nav that was in my app.js to my header component

--- FE/client/src/components/Header.js
+++ FE/client/src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Route, Link, withRouter } from 'react-router-dom';
 
 
 const Header = (props) => {
   return (
     <div>
-      <h1>
-        Lettuce Eat!
-      </h1>
+      {props.currentUser ?
+        <div>
+          <h1>Welcome, {props.currentUser.name}</h1>
+          <button onClick={this.handleLogout}>Logout</button>
+        </div>
+        :
+        <nav>
+          <ul>
+            <Link to="/register"> <li>Register</li></Link>
+            <Link to="/login"> <li>Login</li></Link>
+            <Link to="/recipes"><li> All Recipes</li> </Link>
+            <Link to="/user/recipes"><li> My Recipes</li> </Link>
+          </ul>
+        </nav>
+      }
     </div>
   )
 }
