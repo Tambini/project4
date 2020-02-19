71c24f28d15e06fb50c03f06e154fa9e2583b26f
Tambini <tambini.c@gmail.com>
2020-02-19 02:15:37 -0500

    Added more categories and users in my seed

--- BE/lettuce_eat/db/seeds.rb
+++ BE/lettuce_eat/db/seeds.rb
@@ -20,6 +20,22 @@ Category.create(title:"Cuban")
 Category.create(title:"Spanish")
 Category.create(title:"Japanese")
 
+User.create name: "Carolyn Tambini", email:"user1@test.com", password: "password1"
+User.create name: "Jane Doe", email:"user2@test.com", password: "password2"
+User.create name: "John Snow", email:"user3@test.com", password: "password3"
+User.create name: "John Smith", email:"user4@test.com", password: "password4"
+User.create name: "Joe Schmo", email:"user5@test.com", password: "password5"
+User.create name: "Mary Jane", email:"user1@test.com", password: "password6"
+User.create name: "Jen Smith", email:"user1@test.com", password: "password7"
+User.create name: "Tom Ford", email:"user1@test.com", password: "password8"
+User.create name: "Bob Row", email:"user1@test.com", password: "password9"
+User.create name: "Mike Doe", email:"user1@test.com", password: "password10"
+User.create name: "Jeff Boo", email:"user1@test.com", password: "password11"
+User.create name: "Sam Wright", email:"user1@test.com", password: "password12"
+User.create name: "Paul Salis", email:"user1@test.com", password: "password13"
+User.create name: "Kate Dean", email:"user1@test.com", password: "password14"
+User.create name: "Alice Hall", email:"user1@test.com", password: "password15"
+
 
 
 
