00b42afc277db2f8bbe5ab4fda77c3652b5c2294
Tambini <tambini.c@gmail.com>
2020-02-20 00:26:24 -0500

    Updated homepage carousel

--- FE/client/src/components/Homepage.js
+++ FE/client/src/components/Homepage.js
@@ -1,53 +1,48 @@
-import React, { Component } from "react";
-import EmblaCarouselReact from "embla-carousel-react";
+import React, { useState } from "react";
+import ItemsCarousel from "react-items-carousel";
+import AllRecipes from "./AllRecipes";
 
 import Italian from "../images/italian.jpg";
 import Pie from "../images/pie.jpg";
 import Pizza from "../images/pizza.jpg";
 import Cupcake from "../images/cupcake.jpg";
 import Pineapple from "../images/pineapple.jpg";
-import { withRouter } from "react-router-dom";
+import Steak from "../images/steak.jpg";
 
-import AllRecipes from "./AllRecipes";
-
-class EmblaCarouselComponent extends Component {
-  componentDidMount() {
-    this.embla.on("select", () => {
-      console.log(`Current index is ${this.embla.selectedScrollSnap()}`);
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <EmblaCarouselReact
-          emblaRef={c => (this.embla = c)}
-          options={{ loop: true }}
-        >
-          <div style={{ display: "flex" }}>
-            <div style={{ flex: "75%" }}>
-              <img className="carousel-image" src={Pizza} />
-            </div>
-            <div style={{ flex: "75%" }}>
-              <img className="carousel-image" src={Italian} />
-            </div>
-            <div style={{ flex: "75%" }}>
-              <img className="carousel-image" src={Pineapple} />
-            </div>
-            <div style={{ flex: "75%" }}>
-              <img className="carousel-image" src={Pie} />
-            </div>
-            <div style={{ flex: "75%" }}>
-              <img className="carousel-image" src={Cupcake} />
-            </div>
-          </div>
-        </EmblaCarouselReact>
-        <button onClick={() => this.embla.scrollPrev()}>Prev</button>
-        <button onClick={() => this.embla.scrollNext()}>Next</button>
-        <AllRecipes />
-      </>
-    );
-  }
-}
-
-export default withRouter(EmblaCarouselComponent);
+export default () => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
+  return (
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+      <h1 className="featured-items">Featured Items</h1>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex}
+        activeItemIndex={activeItemIndex}
+        numberOfCards={2}
+        gutter={10}
+        leftChevron={<li className="arrows">{"<"}</li>}
+        rightChevron={<li className="arrows">{">"}</li>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+      >
+        <div style={{ height: 600, background: "#EEE" }}>
+          <img className="carousel-image" src={Cupcake} />
+        </div>
+        <div style={{ height: 600, background: "#EEE" }}>
+          <img className="carousel-image" src={Pizza} />
+        </div>
+        <div style={{ height: 600, background: "#EEE" }}>
+          <img className="carousel-image" src={Italian} />
+          <img className="carousel-image" src={Pineapple} />
+        </div>
+        <div style={{ height: 600, background: "#EEE" }}>
+          <img className="carousel-image" src={Pie} />
+        </div>
+        <div style={{ height: 600, background: "#EEE" }}>
+          <img className="carousel-image" src={Steak} />
+        </div>
+      </ItemsCarousel>
+      <AllRecipes />
+    </div>
+  );
+};
